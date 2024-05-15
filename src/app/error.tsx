b0f37a93f0fbb7d9858a51eccc013a8bd49c1538
LW-Htet-Aung <kiritokirikaya80@gmail.com>
2024-05-16 00:11:49 +0630

    Jobket - update ui

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client'
+const Error = () => {
+    return (
+        <main className="main text-center flex-center space-y-5 flex-col min-h-screen">
+            <h1 className="header-1">Error</h1>
+            <p className="text-muted-foreground">An unexpected error occured</p>
+        </main>
+    )
+}
+export default Error
